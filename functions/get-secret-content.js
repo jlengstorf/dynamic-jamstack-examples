6144d0e4ca9dc89652664f588473f5cf8ff001f6
Jason Lengstorf <jason@lengstorf.com>
2019-12-05 15:55:56 -0800

    feat: add auth example

---
+++ functions/get-secret-content.js
@@ -0,0 +1,27 @@
+exports.handler = async (event, context) => {
+  try {
+    const { user } = context.clientContext;
+
+    if (!user) throw new Error('Not Authorized');
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: `
+        <h1>You’re Invited, ${user.user_metadata.full_name}!</h1>
+        <p>If you can read this it means we’re best friends.</p>
+        <p>
+          Here are the secret details for my birthday party:<br />
+          <a href="https://jason.af/party">jason.af/party</a>
+        </p>
+      `,
+    };
+  } catch (error) {
+    return {
+      statusCode: 401,
+      body: 'Not Authorized',
+    };
+  }
+};
