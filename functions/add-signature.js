8f3ca3c4dd4ff7594bd3feb8efadcecac55925bb
Jason Lengstorf <jason@lengstorf.com>
2019-12-06 15:12:13 -0800

    feat: remove redundant example

--- functions/add-signature.js
+++ functions/add-signature.js
@@ -1,41 +1,30 @@
 const qs = require('querystring');
 const graphql = require('./util/graphql');
 
-const ADD_SIGNATURE = `
-  mutation($signature: String!) {
-    createSignature(data: { name: $signature }) {
-      _id
-    }
-  }
-`;
-
 exports.handler = async event => {
   try {
-    // get the signature from the form data
+    // get the signature from the POST data
     const { signature } = qs.parse(event.body);
+
+    const ADD_SIGNATURE = `
+      mutation($signature: String!) {
+        createSignature(data: { name: $signature }) {
+          _id
+        }
+      }
+    `;
 
     // store the signature in the database
     await graphql(ADD_SIGNATURE, { signature });
 
     // send people back to the petition page
     return {
-      statusCode: 200,
+      statusCode: 302,
       headers: {
-        'Content-Type': 'text/html',
+        Location: '/03-store-data/',
       },
-      body: `
-        <!doctype html>
-        <html>
-          <head>
-            <meta http-equiv="Refresh" content="0; url=/03-store-data" />
-          </head>
-          <body>
-            <p>
-              Redirecting <a href="/03-store-data">back to the petition</a>...
-            </p>
-          </body>
-        </html>
-      `,
+      // body is unused in 3xx codes, but required in all function responses
+      body: 'redirecting...',
     };
   } catch (error) {
     return {
--- functions/get-signatures.js
+++ functions/get-signatures.js
@@ -1,17 +1,15 @@
 const graphql = require('./util/graphql');
 
-const GET_SIGNATURES = `
-  query {
-    signatures {
-      data {
-        name
+exports.handler = async () => {
+  const { signatures } = await graphql(`
+    query {
+      signatures {
+        data {
+          name
+        }
       }
     }
-  }
-`;
-
-exports.handler = async () => {
-  const { signatures } = await graphql(GET_SIGNATURES);
+  `);
 
   return {
     statusCode: 200,
